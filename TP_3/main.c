6a1abf151065d8f3ecb18d6f013c7db393119f12
FabrizioMaggiaiuolo <fabriziomaggia@gmail.com>
2022-06-28 23:35:53 -0300

    rehecho

--- TP_3/main.c
+++ TP_3/main.c
@@ -9,17 +9,9 @@
 #include "controlId.h"
 
 /****************************************************
-    Menu:
-     1. Cargar los datos de los pasajeros desde el archivo data.csv (modo texto).
-     2. Cargar los datos de los pasajeros desde el archivo data.csv (modo binario).
-     3. Alta de pasajero
-     4. Modificar datos de pasajero
-     5. Baja de pasajero
-     6. Listar pasajeros
-     7. Ordenar pasajeros
-     8. Guardar los datos de los pasajeros en el archivo data.csv (modo texto).
-     9. Guardar los datos de los pasajeros en el archivo data.csv (modo binario).
-    10. Salir
+Maggiaiuolo, Fabrizio Julián
+Division C
+Legajo 112952
 *****************************************************/
 
 
